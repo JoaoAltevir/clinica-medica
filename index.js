ea0d48376e557f38f098682c1833915e828a489e
Neto <j.altevirneto@gmail.com>
2024-06-25 08:45:10 -0300

    feat: processo de leitura

---
+++ index.js
@@ -0,0 +1,7 @@
+let mensagemInicial = "Seja bem-vindo! O que você deseja fazer agora?\n\t1 - Adicionar consulta\n\t2 - Listar todas as consultas\n\t3 - Atualizar consultas\n\t4 - Cancelar uma consulta\n\t5 - Sair\n";
+let opcao;
+console.log(mensagemInicial);
+process.stdin.on("data", function (data){
+    let input = data.toString().trim();
+
+})
