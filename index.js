0b380bc7d3a20b5444036ad84e5ec51300711ec2
Neto <j.altevirneto@gmail.com>
2024-06-25 11:36:52 -0300

    feat: add processo de cancelar, porém com falha

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//variaveis-------------------------------------
 let mensagemInicial =
   "Seja bem-vindo! O que você deseja fazer agora?\n\t1 - Adicionar consulta\n\t2 - Listar todas as consultas\n\t3 - Atualizar consultas\n\t4 - Cancelar uma consulta\n\t5 - Sair\n";
 let opcao = 0;
@@ -10,14 +11,23 @@ let paciente = {
 };
 let entradaUsuario;
 const CONSULTAS = [];
+let achou = 0;
+let indice;
+let excluirUsuario;
+let escolhaUsuario;
+let rodou = 0;
+//----------------------------------------------
 console.log(mensagemInicial);
 process.stdin.on("data", function (data) {
+  let input = data.toString().trim();
   if (opcao == 0) {
-    opcao = +data.toString().trim();
+    opcao = +input;
     if (opcao == 1) {
       console.log("Informe o nome do paciente:");
     }else if(opcao == 2){
       console.log("Pressione ENTER para aparecer")
+    }else if(opcao == 4){
+      console.log("Informe o nome do paciente que deseja cancelar:")
     }else if(opcao == 5){
       console.log("Deseja mesmo sair?")
     }
@@ -27,16 +37,16 @@ process.stdin.on("data", function (data) {
 //processo de adicionar consultas    
       case 1:
         if (!paciente.nome) { 
-          paciente.nome = data.toString().trim();
+          paciente.nome = input;
           console.log("Agora informe qual médico irá atendê-lo:");
         } else if (!paciente.medico) {
-          paciente.medico = data.toString().trim();
+          paciente.medico = input;
           console.log("Informe a data da consulta:");
         } else if (!paciente.data) {
-          paciente.data = data.toString().trim();
+          paciente.data = input;
           console.log("Por fim, qual o horário da consulta?");
         } else if (!paciente.hora) {
-          paciente.hora = data.toString().trim();
+          paciente.hora = input;
           console.log("Consulta cadastrada com sucesso!");
           paciente.status = "Agendado"
           CONSULTAS.push(paciente);
@@ -62,6 +72,44 @@ process.stdin.on("data", function (data) {
           opcao = 0;
         break;
 //-------------------------------------------------------------------------
+//processo de exclusão
+        case 4:
+          if(!excluirUsuario){
+            excluirUsuario = data.toString().trim();
+            console.log("Pressione ENTER para continuar")
+          }else if(rodou == 0){
+          for(let i = 0; i < CONSULTAS.length;i++){
+              if(excluirUsuario == CONSULTAS[i].nome){
+                console.log("Registro", i, "\n", CONSULTAS[i]);
+                indice = i;
+                achou++
+                rodou = 1;
+              }
+            }if(achou > 1){//if caso tenha mais de um paciente com o mesmo nome
+              console.log("Qual registro você deseja apagar?")
+              if(!escolhaUsuario){
+                escolhaUsuario = +data.toString().trim();
+              }else{
+                console.log("Consulta cancelada!");
+                CONSULTAS[escolhaUsuario].status = "Cancelado";
+                rodou = 0;
+                opcao = 0;
+                console.log(mensagemInicial);
+              }
+            }else if(achou == 1){//if caso tenha encontrado somente 1 paciente com o nome;
+              console.log("Consulta cancelada!")
+              CONSULTAS[indice].status = "Cancelado"
+              opcao = 0;
+              console.log(mensagemInicial);
+            }else{
+              console.log("Registro não encontrado!\n");
+              opcao = 0;
+              console.log(mensagemInicial);
+            }
+          }
+          break;
+          
+//-------------------------------------------------------------------------
 //processo de saída e confirmação de saída
         case 5:
           if(!entradaUsuario){
@@ -80,6 +128,9 @@ process.stdin.on("data", function (data) {
           break;
 //-------------------------------------------------------------------------
       default:
+        console.log("Opção inválida, tente novamente...");
+        opcao = 0;
+        console.log(mensagemInicial)
         break;
     }
   }
